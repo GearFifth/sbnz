56638e3147563984d3a6ac1a4ff5c0f672777a54
Ilija <ilija.beslin@gmail.com>
2025-09-06 14:32:19 +0200

    back and front initialization

---
+++ slovenia-guide-backend/src/main/java/com/slovenia/guide/SloveniaGuideBackendApplication.java
@@ -0,0 +1,13 @@
+package com.slovenia.guide;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SloveniaGuideBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SloveniaGuideBackendApplication.class, args);
+	}
+
+}
---
+++ slovenia-guide-backend/src/test/java/com/slovenia/guide/SloveniaGuideBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.slovenia.guide;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SloveniaGuideBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
